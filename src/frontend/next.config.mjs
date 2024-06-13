b05a6b301822858826859ddf30e436c1ad07c486
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-13 11:16:33 +0200

    typos

--- src/frontend/next.config.mjs
+++ src/frontend/next.config.mjs
@@ -1,4 +1,11 @@
 export default {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
