c11bdac3f4424b126e0dd66fe69ce80add2fa635
Remco Stoeten <remcostoeten@hotmail.com>
2024-06-13 12:13:37 +0200

    proper location of diary

---
+++ src/frontend/src/app/my-pokemon/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <p className="text-arcady-accent grid place-items-center h-screen w-scren bg-arcady-bg">
+      Please come back next sprint.
+    </p>
+  );
+}
